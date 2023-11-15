b60ba3a06df76b35c4df69619107b5c2cce94744
HiramMcc <hirammisael.cerdacasas@gmail.com>
2023-11-14 18:13:00 -0600

    Commit
    Cambio de bootn de register

--- PIA-SUPREME-SAVOR/src/app/login/login.page.ts
+++ PIA-SUPREME-SAVOR/src/app/login/login.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { NavController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-login',
@@ -13,13 +15,16 @@ export class LoginPage implements OnInit {
     password: new FormControl('', [Validators.required])
   })
 
-  constructor() {
+  constructor(private navCtrl: NavController) {
 
      
 
    }
 
   ngOnInit() {
+  }
+  cambiarPagina(pagina: string){
+    this.navCtrl.navigateForward(pagina);
   }
 
 }
